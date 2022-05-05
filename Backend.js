98de36374a35da0114804d8bcdb2099a02ce732d
Reed <86171134+not-reed@users.noreply.github.com>
2022-05-04 17:36:55 -0700

    Update Backend.js
    typo in example

--- Backend.js
+++ Backend.js
@@ -4,7 +4,7 @@ router.get("/:address", async (req, res) => {
 		"0x8d12a197cb00d4456a1fe03395095ce2a5cc6819",
 		"0x8d12a197cb00d4123a1fe03395095ce2a5cc6819",
 	];
-	const leafNodes = whitelist.map((address) => keccak256(req.params.address));
+	const leafNodes = whitelist.map((address) => keccak256(address));
 	const tree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 	let proof = tree.getHexProof(keccak256(req.params.address));
 	res.json({ proof });
